e83c6628ccfb6086c11f14ea6d193ccddacf2abd
Alejandro Pacheco Bobillo <alepacheco99@gmail.com>
2016-06-07 19:00:25 +0200

    buttons are working and do diferent things based on a swich statement
    TODO: improve bunnton handling

--- mobile/src/main/java/com/company/studyless/MainActivity.java
+++ mobile/src/main/java/com/company/studyless/MainActivity.java
@@ -2,20 +2,45 @@ package com.company.studyless;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RadioButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    RadioButton mbutton1a;
+    Button b1a, b1b, b1c, b1d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mbutton1a = findViewById(R.id.radioButton1a);
+        b1a = (Button) findViewById(R.id.B1a);
+
+
+    }
+
+
+    public void buttonclicklistener(View v) {
+        String text = "bad";
+        switch (v.getId()) {
+            case R.id.B1a:
+                text = "a";
+                break;
+            case R.id.B1b:
+                text = "b";
+                break;
+            case R.id.B1c:
+                text = "c";
+                break;
+        }
+
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
 
 
 
     }
+
 }
+
+
+
