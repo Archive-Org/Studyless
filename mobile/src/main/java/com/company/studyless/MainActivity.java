4bb95b54a7d5477f7796b31ac6b9b7a061f1cf9b
Alejandro Pacheco Bobillo <alepacheco99@gmail.com>
2016-06-28 17:16:54 +0200

    Database and matrix sync in a two-way realtime manner
    //TODO rethink about using or not a matrix
    
    *WORKING*

--- mobile/src/main/java/com/company/studyless/MainActivity.java
+++ mobile/src/main/java/com/company/studyless/MainActivity.java
@@ -13,15 +13,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.json.JSONException;
-
-import java.util.Arrays;
-
 public class MainActivity extends AppCompatActivity {
     RadioGroup G1, G2, G3, G4, G5;
-    TextView resultado1, resultado2, resultado3, resultado4, resultado5;
+    TextView result1, result2, result3, result4, result5, matrixText;
     DatabaseReference mDatabase;
     matrix matrix = new matrix();
+    int room = 4353;
 
 
     @Override
@@ -29,20 +26,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
         G1 = (RadioGroup) findViewById(R.id.radioGroup1);
-        resultado1 = (TextView) findViewById(R.id.resultado1);
-        resultado2 = (TextView) findViewById(R.id.resultado2);
-        resultado3 = (TextView) findViewById(R.id.resultado3);
-        resultado4 = (TextView) findViewById(R.id.resultado4);
-        resultado5 = (TextView) findViewById(R.id.resultado5);
+        G2 = (RadioGroup) findViewById(R.id.radioGroup2);
+        G3 = (RadioGroup) findViewById(R.id.radioGroup3);
+        G4 = (RadioGroup) findViewById(R.id.radioGroup4);
+        G5 = (RadioGroup) findViewById(R.id.radioGroup5);
+        result1 = (TextView) findViewById(R.id.resultado1);
+        result2 = (TextView) findViewById(R.id.resultado2);
+        result3 = (TextView) findViewById(R.id.resultado3);
+        result4 = (TextView) findViewById(R.id.resultado4);
+        result5 = (TextView) findViewById(R.id.resultado5);
+        matrixText = (TextView) findViewById(R.id.matrixText);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-
+        initializeRoom();
         //Read from Database
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                matrix.SyncWDB(dataSnapshot.child("room_" + room).getValue());
+                matrixText.setText(matrix.matrix2string(matrix.getData(), 5, 4));
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
@@ -50,24 +52,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void buttonclicklistener(View v) throws JSONException {
+    public void buttonClickListener(View v) {
         String tag = (String) v.getTag();
         String[] parts = tag.split(",");
         int row = Integer.parseInt(parts[0]);
-        int colum = Integer.parseInt(parts[1]);
-        matrix.addOne(row, colum);
-        String text = row + " " + colum;
-        updateRows(4353);
+        int column = Integer.parseInt(parts[1]);
+        //matrix.addOne(row, column);
+
+
+        updateEntry(row, column);
+
+
+        String text = (1 + row) + "-" + (1 + column);
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
 
     }
 
-    public void updateRows(int room) {
-        mDatabase.child("room_" + room + "/0").setValue(Arrays.toString(matrix.getData(0)));
-        mDatabase.child("room_" + room + "/1").setValue(Arrays.toString(matrix.getData(1)));
-        mDatabase.child("room_" + room + "/2").setValue(Arrays.toString(matrix.getData(2)));
-        mDatabase.child("room_" + room + "/3").setValue(Arrays.toString(matrix.getData(3)));
-        mDatabase.child("room_" + room + "/4").setValue(Arrays.toString(matrix.getData(4)));
+
+    public void updateEntry(int row, int column) {
+        int value = matrix.getData()[row][column] + 1;
+        mDatabase.child("room_" + room + "/" + row + "/" + column + "").setValue(value);
+
+
 
     }
+
+    public void initializeRoom() {
+        int e = 0;
+        int i = 0;
+        while (e < 5) {
+            while (i < 4) {
+                mDatabase.child("room_" + room + "/" + e + "/" + i + "").setValue(0);
+                i++;
+            }
+            i = 0;
+            e++;
+
+        }
+    }
+
 }
--- mobile/src/main/java/com/company/studyless/matrix.java
+++ mobile/src/main/java/com/company/studyless/matrix.java
@@ -1,8 +1,7 @@
 package com.company.studyless;
 
-/**
- * Created by mac on 28/6/16.
- */
+import java.util.ArrayList;
+
 public class matrix {
     private int[][] Data = new int[5][4];
 
@@ -10,15 +9,52 @@ public class matrix {
         return Data;
     }
 
+    public void SyncWDB(Object dbValue) {
+        ArrayList level1 = (ArrayList) dbValue;
+        int e = 0;
+        int i = 0;
+        while (e < 5) {
+            ArrayList esto = (ArrayList) level1.get(e);
+            while (i < 4) {
+                Long esto2 = Long.parseLong(String.valueOf(esto.get(i)));
+                int esto3 = esto2.intValue();
+                Data[e][i] = esto3;
+
+                i++;
+            }
+            i = 0;
+            e++;
+        }
+
+    }
+
     public int[] getData(int row) {
         return Data[row];
     }
 
-    public void addOne(int row, int colum) {
-        this.Data[row][colum] += 1;
+    public void addOne(int row, int column) {
+        this.Data[row][column] += 1;
     }
 
-    public void restOne(int row, int colum) {
-        this.Data[row][colum] -= 1;
+    public void restOne(int row, int column) {
+        this.Data[row][column] -= 1;
+    }
+
+    public String matrix2string(int[][] matrix2convert, int i, int e) {
+        String converted = "";
+        int ii = 0;
+        int ee = 0;
+        while (ii < i) {
+            converted += "\n";
+            while (ee < e) {
+                converted += matrix2convert[ii][ee] + " ";
+                ee++;
+
+
+            }
+            ee = 0;
+            ii++;
+        }
+        return converted;
     }
 }
